c35d7ccd9b021ba75bc92604cc39817b0efcb2fc
Wilton Ribeiro <wiltonribeiromaster@gmail.com>
2021-03-10 00:22:48 -0300

    checar a existência de valores em cookies

--- js/reload.js
+++ js/reload.js
@@ -1,6 +1,12 @@
-function redirect(url){
+function _redirect(url){
   // if url does not exist redirect to the page itself
   if (!url) url = window.location
 
   window.location = url;
+}
+
+function _checkStorage(){
+  let lydia = sessionStorage.getItem("lydia");
+  // the value is stored as String so the comparison needs to be between Strings
+  return (lydia == "true")
 }
